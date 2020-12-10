5c7c9632cc0c041b35aa79d528cd91543979b7b8
Victor de Coninck <victor.de-coninck@epita.fr>
2020-12-10 18:17:12 +0100

    adding send-alert fct to send mail

--- src/main.py
+++ src/main.py
@@ -4,10 +4,16 @@ import RPi.GPIO as GPIO
 from w1thermsensor import W1ThermSensor
 import src.i2c_demo as i2c
 
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 calibration = ""
+aquariophile = "abraham.sylla@epita.net"
 
 tempg = 4
 tempr = 27
@@ -23,28 +29,59 @@ phmin = 8
 phmax = 8.4
 
 
+def send_alert(sonde, niveau)
+    receiver_email = aquariophile  
+    sender_email = "w.world2345@laposte.net" 
+    password = 'Annecy2019!'
+
+    msg = MIMEMultipart()
+    msg["Subject"]= "I am a fish and I need air"
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    message = "le niveau de " + sonde + " est à " + niveau
+    msg.attach(MIMEText(message))
+    mailserver = smtplib.SMTP('smtp.laposte.net', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+    mailserver.login(sender_email, password)
+    try:
+       di = mailserver.sendmail(sender_email, e, msg.as_string())
+       print(di)
+    except:
+       print('mail not sent')
+    mailserver.quit()
+
+
+
 def settempled(value):
     if value > tempmax:
         GPIO.output(tempr, 1)
         GPIO.output(tempg, 0)
+        send_alert("température", value)
     elif value < tempmin:
         GPIO.output(tempr, 1)
         GPIO.output(tempg, 0)
+        send_alert("température", value)
     else:
         GPIO.output(tempr, 0)
         GPIO.output(tempg, 1)
-
 
 def setphled(value):
     if value > phmax:
         GPIO.output(phr, 1)
         GPIO.output(phg, 0)
+        send_alert("ph", value)
     elif value < phmin:
         GPIO.output(phr, 1)
         GPIO.output(phg, 0)
+        send_alert("ph", value)
     else:
         GPIO.output(phr, 0)
         GPIO.output(phg, 1)
+
+
+
 
 
 if __name__ == '__main__':
